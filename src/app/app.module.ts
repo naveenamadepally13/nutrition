264d9e383e3dde96019e0dc3081e2a04115cdbad
Neha Navgale <nn78d@mail.umkc.edu>
2019-02-06 19:13:26 -0600

    Added register component and added routing between login and register component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NutritionComponent } from './nutrition/nutrition.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NutritionComponent
+    NutritionComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
-
+  emailAddress;
+  passWord;
+  userKey = 'userName';
+  passwordKey = 'password';
+  textToSpeech1;
+  textToSpeech;
+  loginCreation(event) {
+    if (localStorage.length > 0) {
+      const localKey = localStorage.getItem(this.userKey);
+      if (localKey !== this.emailAddress) {
+        localStorage.setItem(this.userKey, this.emailAddress);
+        localStorage.setItem(this.passwordKey, this.passWord);
+      }
+    }
+  }
   ngOnInit() {
   }
 
