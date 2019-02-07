58d35726b4effe6f2fdb98c15447562cd0dce3f4
Neha Navgale <nn78d@mail.umkc.edu>
2019-02-06 23:14:25 -0600

    Implemented logic for register and Login page

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,7 +8,39 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   constructor() { }
+  registerMessage;
+  firstNameKey = 'FirstName';
+  firstName;
+  lastNameKey = 'LastName';
+  lastName;
+  emailAddressKey = 'EmailAddress';
+  emailAddress;
+  passwordKey = 'Password';
+  password;
+  getEmailAddress;
+  dialogDisplay = 'none';
+  registerAccount(event) {
+    this.getEmailAddress = localStorage.getItem(this.emailAddress);
+    if (this.getEmailAddress !== this.emailAddress) {
+      localStorage.setItem(this.firstNameKey, this.firstName);
+      localStorage.setItem(this.lastNameKey, this.lastName);
+      localStorage.setItem(this.emailAddressKey, this.emailAddress);
+      localStorage.setItem(this.passwordKey, this.password);
+      this.registerMessage = 'Your account has been registered. Please enter your Email Address and Password on Login page.';
+      this.openModalDialog();
+    } else {
+      this.registerMessage = 'You already have an account with Email Address {{emailAddress}}. Please click on OK button to navigate ' +
+        'to Login Page.';
+    }
+  }
 
+  openModalDialog() {
+    this.dialogDisplay = 'block';
+  }
+
+  closeModalDialog() {
+    this.dialogDisplay = 'none';
+  }
   ngOnInit() {
   }
 
